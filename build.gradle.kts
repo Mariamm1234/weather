ffb1d287594b14b14b0e4ba5951b51637c58a6eb
AlfaroukOmar <92135352+AlfaroukOmar@users.noreply.github.com>
2024-11-02 18:55:52 +0200

    fix(): dagger build number

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,14 @@ buildscript {
         classpath ("com.android.tools.build:gradle:3.4.0")
         classpath ("com.google.gms:google-services:4.3.10")
         classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.52")
+
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
 
 }
